49469cc0447c47e876b1fe3616b38dd6879d2feb
jason1466 <jason1466@users.noreply.github.com>
2019-03-01 16:23:55 -0500

    addBearerToken

--- src/app/core/interceptors/http.token.interceptor.ts
+++ src/app/core/interceptors/http.token.interceptor.ts
@@ -5,7 +5,7 @@ import {
   HttpHandler,
   HttpRequest
 } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Observable, from } from "rxjs";
 
 import { JwtService } from "../services";
 import { HttpHeaders, HttpClient, HttpParams } from "@angular/common/http";
@@ -19,46 +19,57 @@ export class HttpTokenInterceptor implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    return from(this.addBearerToken(req, next));
+  }
+
+  private async addBearerToken(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Promise<HttpEvent<any>> {
+    let token: string;
+
     const headersConfig = {
       "Content-Type": "application/json",
       Accept: "application/json"
     };
 
-    let token: Promise<string>; // = req.headers.get("x-ms-token-aad-access_token");
-    console.log("x-ms-token-aad-access_token: " + token);
+    // let token: Promise<string>; // = req.headers.get("x-ms-token-aad-access_token");
+    // console.log("x-ms-token-aad-access_token: " + token);
 
     console.log("req.url: " + req.url);
-    console.log("req.url.search result: " + req.url.search(/\b\/\.auth\/me/g));
-    console.log("req.url.indexOf result: " + req.url.indexOf(".auth/me"));
+    // console.log("req.url.search result: " + req.url.search(/\b\/\.auth\/me/g));
+    // console.log("req.url.indexOf result: " + req.url.indexOf(".auth/me"));
 
     if (req.url.search(/\b\/\.auth\/me/g) == -1) {
       // let token = this.jwtService.getToken();
       console.log("/.auth/me not found in req.url: ");
-      token = this.getAuthMe().then(data => data as string);
-      // console.log("back from getMe.then() data result: " + data);
-      // token = data;
+      await this.http
+        .get<any>("https://teamwizapp.azurewebsites.net/.auth/me")
+        .subscribe(x => {
+          token = x.access_token;
+          console.log(
+            "back from getMe.subscribe() access_token result: " + token
+          );
+        });
     }
 
     if (token) {
-      headersConfig["Authorization"] = `Bearer ${token.then(t => t as string)}`;
+      headersConfig["Authorization"] = `Bearer ${token}`;
       // headersConfig['Authorization'] = `Token ${token}`;
     }
 
-    console.log(
-      "exiting HttpInterceptor, token is: " + token.then(t => t as string)
-    );
+    console.log("exiting HttpInterceptor, token is: " + token);
     const request = req.clone({ setHeaders: headersConfig });
     return next.handle(request);
   }
 
-  async getAuthMe(): Promise<string> {
-    // let bar = this.http.post("https://teamwizapp.azurewebsites.net/.auth/login/google", { "id_token": "<id_token>")
-    let foo = await this.http
-      .get<any>("https://teamwizapp.azurewebsites.net/.auth/me")
-      .toPromise()
-      .then(data => data.access_token as string);
-    // return data.access_token;
-    // console.log("in getMe.then() data result: " + foo);
-    return foo;
-  }
+  // getAuthMe(): Observable<string> {
+  //   // let bar = this.http.post("https://teamwizapp.azurewebsites.net/.auth/login/google", { "id_token": "<id_token>")
+  //   return this.http
+  //     .get<any>("https://teamwizapp.azurewebsites.net/.auth/me");
+  //     // .subscribe(data => data.access_token as string);
+  //   // return data.access_token;
+  //   // console.log("in getMe.then() data result: " + foo);
+  //   // return foo;
+  // }
 }
