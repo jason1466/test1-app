3112065d3cd424887c1e26160c23d748f8a0543d
jason1466 <jason1466@users.noreply.github.com>
2019-03-01 12:16:45 -0500

    token async await 13

--- src/app/core/interceptors/http.token.interceptor.ts
+++ src/app/core/interceptors/http.token.interceptor.ts
@@ -28,9 +28,10 @@ export class HttpTokenInterceptor implements HttpInterceptor {
     console.log("x-ms-token-aad-access_token: " + token);
 
     console.log("req.url: " + req.url);
-    console.log("req.url.search result: " + req.url.search(/\b\/.auth\/me/g));
+    console.log("req.url.search result: " + req.url.search(/\b\/\.auth\/me/g));
+    console.log("req.url.indexOf result: " + req.url.indexOf(".auth/me"));
 
-    if (req.url.search(/\b\/.auth\/me/g) == -1) {
+    if (req.url.indexOf(".auth/me") == -1) {
       // let token = this.jwtService.getToken();
       console.log("/.auth/me not found in req.url: ");
       this.getAuthMe().then(data => {
@@ -50,7 +51,7 @@ export class HttpTokenInterceptor implements HttpInterceptor {
 
   async getAuthMe(): Promise<string> {
     return await this.http
-      .get<any>("/.auth/me")
+      .get<any>("https://teamwizapp.azurewebsites.net/.auth/me")
       .toPromise()
       .then(data => {
         console.log("in getMe.then()");
