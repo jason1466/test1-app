35fa3fb8b5e95e1caacf47846a21ea8710f412ce
jason1466 <jason1466@users.noreply.github.com>
2019-03-01 08:27:19 -0500

    token async await

--- src/app/core/interceptors/http.token.interceptor.ts
+++ src/app/core/interceptors/http.token.interceptor.ts
@@ -15,7 +15,7 @@ import { map } from "rxjs/operators";
 export class HttpTokenInterceptor implements HttpInterceptor {
   constructor(private jwtService: JwtService, private apiService: ApiService) {}
 
-  intercept(
+  async intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
@@ -29,16 +29,19 @@ export class HttpTokenInterceptor implements HttpInterceptor {
     let token = req.headers.get("x-ms-token-aad-access_token");
     // let token = this.jwtService.getToken();
 
-    this.apiService.get("/.auth/me").pipe(
+    await this.apiService.get("/.auth/me").pipe(
       map(data => {
         token = data.access_token;
-        if (token) {
-          headersConfig["Authorization"] = `Bearer ${token}`;
-          // headersConfig['Authorization'] = `Token ${token}`;
-        }
-        const request = req.clone({ setHeaders: headersConfig });
-        return next.handle(request);
       })
     );
+    if (token) {
+      headersConfig["Authorization"] = `Bearer ${token}`;
+      console.log(".auth/me token: " + token);
+      // headersConfig['Authorization'] = `Token ${token}`;
+    }
+    const request = req.clone({ setHeaders: headersConfig });
+    return next.handle(request);
   }
+
+  // async getMe() {}
 }
